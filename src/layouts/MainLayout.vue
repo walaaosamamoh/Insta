d3864a4334754f5297f46c7f608013595a827d7a
walaaosamamoh <waloosama@gmail.com>
2026-07-06 14:53:17 +0300

    remove navigation array and integrate it from constant file

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,9 +2,9 @@
   <div class="min-h-screen flex flex-col container">
     <router-view :key="$route.fullPath" class="flex-1" />
     <nav
-      class="sticky z-20 bg-white bottom-0 flex flex-row justify-around items-center px-6 py-4 border-t border-t-gray-200"
+      class="sticky z-20 bg-white inset-0 flex flex-row justify-around items-center px-6 py-4 border-t border-t-gray-200"
     >
-      <div v-for="nav in navigation" :key="nav.title">
+      <div v-for="nav in menu" :key="nav.title">
         <router-link :to="nav.to" v-slot="{ isExactActive }">
           <component :is="nav.icon" :is-active="isExactActive" />
         </router-link>
@@ -14,21 +14,12 @@
 </template>
 
 <script>
-import homeSvg from '@/components/icons/homeSvg.vue'
-import searchSvg from '@/components/icons/searchSvg.vue'
-import reelsSvg from '@/components/icons/reelsSvg.vue'
-import ProfileNav from '@/components/profileNav.vue'
-import { markRaw } from 'vue'
+import { menu } from '@/utils/constants'
 
 export default {
   data() {
     return {
-      navigation: [
-        { title: 'home', icon: markRaw(homeSvg), to: { name: 'home' } },
-        { title: 'search', icon: markRaw(searchSvg), to: { name: 'search' } },
-        { title: 'reels', icon: markRaw(reelsSvg), to: { name: 'reels' } },
-        { title: 'profile', icon: markRaw(ProfileNav), to: { name: 'profile' } },
-      ],
+      menu,
     }
   },
 }
