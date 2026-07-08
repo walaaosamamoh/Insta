ac817a9f36121504133dc1c937dd2d124c70f5c4
walaaosamamoh <waloosama@gmail.com>
2026-07-08 14:14:17 +0300

    fix layout on mobile

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,12 @@
 <template>
-  <div class="min-h-screen flex flex-col md:grid md:grid-cols-[80px_1fr]">
+  <div class="h-screen flex flex-col md:grid md:grid-cols-[80px_1fr]">
+    <div class="flex-1 overflow-y-auto md:col-start-2">
+      <div class="md:max-w-2xl mx-auto">
+        <router-view :key="$route.fullPath" />
+      </div>
+    </div>
     <nav
-      class="fixed z-20 bg-white bottom-0 left-0 right-0 flex flex-row justify-around md:flex-col md:gap-8 md:fixed md:left-0 md:top-0 md:w-20 md:h-screen px-6 py-4 border-t border-t-gray-200"
+      class="sticky z-20 bg-white bottom-0 left-0 right-0 flex flex-row justify-around md:flex-col md:gap-8 md:fixed md:left-0 md:top-0 md:w-20 md:h-screen px-6 py-4 border-t border-t-gray-200"
     >
       <div v-for="nav in menu" :key="nav.title">
         <router-link :to="nav.to" v-slot="{ isExactActive }">
@@ -9,9 +14,6 @@
         </router-link>
       </div>
     </nav>
-    <div class="flex-1 md:col-start-2 max-w-2xl mx-auto">
-      <router-view :key="$route.fullPath" />
-    </div>
   </div>
 </template>
 
